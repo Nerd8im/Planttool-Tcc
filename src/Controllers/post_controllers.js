35416e08f1947e8ac86b33235679830edeb2845d
N8im <nerdgamerbro666@gmail.com>
2025-10-20 13:45:14 -0300

    Remove fotoPerfil from Usuario.registrar call

--- src/Controllers/post_controllers.js
+++ src/Controllers/post_controllers.js
@@ -13,7 +13,7 @@ export async function registrarUsuario(req, res) {
     const { nome, sobrenome, email, senha } = req.body
 
     try {
-        const resposta = await Usuario.registrar(nome, sobrenome, email, senha, fotoPerfil)
+        const resposta = await Usuario.registrar(nome, sobrenome, email, senha)
         res.status(200).json(resposta)
 
     } catch (error) {
